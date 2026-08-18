da958d95e772d1cad78062c19e22738298ba5c7c
Sureka R <surekar.aids2024@citchennai.net>
2026-08-18 21:29:01 +0530

    grop ai connection

--- backend/services/explain_service.py
+++ backend/services/explain_service.py
@@ -124,7 +124,7 @@ Final Conclusion:
         try:
 
             response = client.chat.completions.create(
-                model="llama-3.3-70b-versatile",
+                model="llama-3.1-8b-instant",
                 messages=[
                     {
                         "role": "system",
