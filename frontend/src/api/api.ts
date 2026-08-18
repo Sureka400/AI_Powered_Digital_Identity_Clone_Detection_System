586cfe35117ed22f6b396328beef7ababfec90cd
Sureka R <surekar.aids2024@citchennai.net>
2026-08-18 21:39:15 +0530

    Update frontend API configuration

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://digital-identity-backend-8i1s.onrender.com/"
+    baseURL: "http://localhost:8000",
 });
 
 export default api;
