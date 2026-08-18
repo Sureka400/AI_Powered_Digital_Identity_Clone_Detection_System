e42631d727a1111b2698fa561466ddac6d658829
Sureka R <surekar.aids2024@citchennai.net>
2026-08-18 21:58:30 +0530

    recommendation setup

--- backend/services/explain_service.py
+++ backend/services/explain_service.py
@@ -124,7 +124,7 @@ Final Conclusion:
         try:
 
             response = client.chat.completions.create(
-                model="llama-3.1-8b-instant",
+                model="openai/gpt-oss-20b",
                 messages=[
                     {
                         "role": "system",
--- backend/services/recommendation_service.py
+++ backend/services/recommendation_service.py
@@ -128,7 +128,7 @@ Do not include ```json.
         try:
 
             response = client.chat.completions.create(
-                model="llama-3.3-70b-versatile",
+                model="openai/gpt-oss-20b",
                 messages=[
                     {
                         "role": "system",
